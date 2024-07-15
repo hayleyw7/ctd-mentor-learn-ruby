41840725b749146aed073cbce751b81d88c5dc03
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-15 17:05:47 -0600

    Complete 6

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,17 @@
 class Timer
   #write your code here
+
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    hours = @seconds / 3600
+    minutes = (@seconds % 3600) / 60
+    seconds = @seconds % 60
+
+    format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
 end
