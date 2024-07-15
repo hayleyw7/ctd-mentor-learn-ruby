f02c7c8835b30455554635039f0ecee5b0d92cb9
Hayley Witherell <hayleywitherell@gmail.com>
2024-07-15 16:36:23 -0600

    Complete 3-5

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,27 @@
 #write your code here
+
+def translate(words)
+  vowels = ['a', 'e', 'i', 'o', 'u']
+
+  translated_words = words.split.map do |word|
+    first_vowel_index = word.length
+    
+    word.chars.each_with_index do |letter, index|
+      if vowels.include?(letter) && first_vowel_index == word.length
+        if word[index - 1] == 'q' && letter == 'u'
+          first_vowel_index = index + 1
+
+        else
+          first_vowel_index = index
+        end
+      end
+    end
+    
+    start_of_word = word[0, first_vowel_index]
+    rest_of_word = word[first_vowel_index, word.length - first_vowel_index]
+    
+    "#{rest_of_word}#{start_of_word}ay"
+  end
+  
+  translated_words.join(" ")
+end
--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
 # write your code here
+
+  attr_reader :title
+
+  def title=(new_title)
+    little_words = %w[a an and as at but by for if in nor of on or so the to up yet over]
+
+    words = new_title.split.map.with_index do |word, index|
+      if index == 0 || !little_words.include?(word)
+        word.capitalize
+
+      else
+        word
+      end
+    end
+
+    @title = words.join(' ')
+  end
 end
